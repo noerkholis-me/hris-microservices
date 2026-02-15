badda6c6d541af047fff4afd82d681801a481adf
Nurkholis Majid <noerkholis.madjid@gmail.com>
2026-02-15 22:56:54 +0700

    ♻️ refactor(contracts): change example login and register dto

--- packages/contracts/src/dtos/auth/login.dto.ts
+++ packages/contracts/src/dtos/auth/login.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'admin@dot.com', description: 'Email user' })
+  @ApiProperty({ example: 'user@example.com', description: 'Email user' })
   @IsEmail({}, { message: 'Email tidak valid' })
   email: string;
 
-  @ApiProperty({ example: 'admin123', description: 'Password user' })
+  @ApiProperty({ example: 'password123', description: 'Password user' })
   @IsString()
   password: string;
 }
--- packages/contracts/src/dtos/auth/register.dto.ts
+++ packages/contracts/src/dtos/auth/register.dto.ts
@@ -8,23 +8,23 @@ import {
 } from 'class-validator';
 
 export class RegisterDto {
-  @ApiProperty({ example: 'noerkholis@gmail.com', description: 'Email user' })
+  @ApiProperty({ example: 'user@example.com', description: 'Email user' })
   @IsEmail({}, { message: 'Email tidak valid' })
   @IsOptional()
-  email!: string;
+  email: string;
 
   @ApiProperty({
-    example: 'catat.in-nurkholis25',
+    example: 'password123',
     description: 'Password user',
   })
   @IsString()
   @MinLength(8, { message: 'Password minimal 8 karakter' })
   @MaxLength(50, { message: 'Password maksimal 50 karakter' })
-  password!: string;
+  password: string;
 
   @ApiProperty({ example: 'Nurkholis Majid', description: 'Nama lengkap user' })
   @IsString()
   @MinLength(2, { message: 'Nama minimal 2 karakter' })
   @MaxLength(100, { message: 'Nama maksimal 100 karakter' })
-  fullName!: string;
+  fullName: string;
 }
